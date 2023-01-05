855b6eb2f95571dbb08213e8b43e1d15dc98ff6a
Amin Ouled Mohamed <rouga2012@hotmail.fr>
2023-01-05 17:34:20 -0500

    Add Create Triangle

--- OpenGLCourseApp/OpenGLCourseApp.cpp
+++ OpenGLCourseApp/OpenGLCourseApp.cpp
@@ -22,6 +22,13 @@ void CreateTriangle() {
 
 	glGenBuffers(1, &gVBO);
 	glBindBuffer(GL_ARRAY_BUFFER, gVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(wVertices), wVertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 }
 
 int main()
